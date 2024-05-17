0a0b973a94779e75247d4090e0e2c1aabcce0ec5
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-05-17 13:57:19 +0300

    Add  comments

--- spring/src/main/java/com/example/spring/tacos/HomeController.java
+++ spring/src/main/java/com/example/spring/tacos/HomeController.java
@@ -3,6 +3,8 @@ package com.example.spring.tacos;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+/**Класс контроллера, обрабатывающий запросы с корневым путём /
+ * и передаёт их представлению домашней страницы без заполнения модели данных */
 @Controller
 public class HomeController {
     @GetMapping("/")
--- spring/src/test/java/com/example/spring/tacos/HomeControllerTest.java
+++ spring/src/test/java/com/example/spring/tacos/HomeControllerTest.java
@@ -4,19 +4,24 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+/** Тесты для домашней страницы
+ * */
 @WebMvcTest(HomeController.class)
 public class HomeControllerTest {
 
+    /* Имитируем работу Spring MVC, чтобы не запускать сервер */
     @Autowired
     private MockMvc mockMvc;
 
+    /** Выполняем HTTP-запрос GET с корневым путём / и затем убедимся, что контроллер выбрал имя представление
+     * home, а сама страница содержит фразу "Welcome to..."
+     * */
     @Test
     public void testHomePage() throws Exception {
         mockMvc.perform(get("/"))
