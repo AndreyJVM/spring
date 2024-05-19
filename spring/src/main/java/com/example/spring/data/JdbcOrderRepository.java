c66dff203f05b887bd396ab058541a96f5f297d3
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-05-19 09:41:36 +0300

    Update JdbsOrderRepository

--- spring/src/main/java/com/example/spring/data/JdbcOrderRepository.java
+++ spring/src/main/java/com/example/spring/data/JdbcOrderRepository.java
@@ -1,8 +1,16 @@
 package com.example.spring.data;
 
+import com.example.spring.tacos.Taco;
 import com.example.spring.tacos.TacoOrder;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.PreparedStatementCreator;
+import org.springframework.jdbc.core.PreparedStatementCreatorFactory;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.transaction.annotation.Transactional;
+import java.sql.Types;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 public class JdbcOrderRepository implements OrderRepository{
 
@@ -15,6 +23,45 @@ public class JdbcOrderRepository implements OrderRepository{
     @Override
     @Transactional
     public TacoOrder save(TacoOrder tacoOrder) {
-        return null;
+        PreparedStatementCreatorFactory pscf = new PreparedStatementCreatorFactory(
+          "insert into Taco_Order "
+          + "(delivery_name, delivery_street, delivery_city, " +
+                  "delivery_state, delivery_zip, cc_number, " +
+                  "cc_expiration, cc_cvv, placed_at) " +
+                  "values (?,?,?,?,?,?,?,?,?)",
+                Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,
+                Types.VARCHAR, Types.VARCHAR, Types.VARCHAR,
+                Types.VARCHAR, Types.VARCHAR, Types.TIMESTAMP
+        );
+        pscf.setReturnGeneratedKeys(true);
+
+        tacoOrder.setPlacedAt(new Date());
+        PreparedStatementCreator psc = pscf.newPreparedStatementCreator(
+                Arrays.asList(
+                        tacoOrder.getDeliveryName(),
+                        tacoOrder.getDeliveryStreet(),
+                        tacoOrder.getDeliveryCity(),
+                        tacoOrder.getDeliveryState(),
+                        tacoOrder.getDeliveryZip(),
+                        tacoOrder.getCcNumber(),
+                        tacoOrder.getCcExpiration(),
+                        tacoOrder.getCcCVV(),
+                        tacoOrder.getPlacedAt()));
+
+        GeneratedKeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcOperations.update(psc, keyHolder);
+        long orderId = keyHolder.getKey().longValue();
+        tacoOrder.setId(orderId);
+
+        List<Taco> tacos = tacoOrder.getTacos();
+        int i = 0;
+        for (Taco taco : tacos){
+            saveTaco(orderId, i++, taco);
+        }
+        return tacoOrder;
+    }
+
+    private void saveTaco(long orderId, int orderKey, Taco taco) {
+
     }
 }
