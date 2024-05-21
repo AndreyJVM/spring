46365c0ad5b392249b4fd78ec56caff0399e12bd
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-05-21 07:49:07 +0300

    Add new annotations @Table and @Id

--- spring/src/main/java/com/example/spring/tacos/Ingredient.java
+++ spring/src/main/java/com/example/spring/tacos/Ingredient.java
@@ -1,14 +1,22 @@
 package com.example.spring.tacos;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 /** У каждого ингредиента есть название и тип, позволяющие его визуально квалифицировать
  * */
 @Data
+@Table
 public class Ingredient {
 
+    @Id
     private final String id;
+
+    @Id
     private final String name;
+
+    @Id
     private final Type type;
 
     public enum Type {
--- spring/src/main/java/com/example/spring/tacos/TacoOrder.java
+++ spring/src/main/java/com/example/spring/tacos/TacoOrder.java
@@ -5,6 +5,8 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.CreditCardNumber;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Table;
 
 import java.io.Serializable;
 import java.util.ArrayList;
@@ -17,36 +19,47 @@ import java.util.List;
  */
 
 @Data
+@Table("Taco_Order")
 public class TacoOrder implements Serializable {
 
     private static final long serialVersionID = 1L;
 
+    @Id
     private Long id;
 
+    @Id
     private Date placedAt;
 
+    @Id
     @NotBlank(message = "Delivery name is required")
     private String deliveryName;
 
+    @Id
     @NotBlank(message = "Street is required")
     private String deliveryStreet;
 
+    @Id
     @NotBlank(message = "City is required")
     private String deliveryCity;
 
+    @Id
     @NotBlank(message = "State is required")
     private String deliveryState;
 
+    @Id
     @NotBlank(message = "Zip code is required")
     private String deliveryZip;
 
+    @Id
     @CreditCardNumber(message = "Not a valid credit card number")
     private String ccNumber;
 
+    @Id
     @Pattern(regexp = "^(0[1-9]|1[0-2])([\\/])([2-9][0-9])$",
             message = "Must be formatted MM/YY")
     private String ccExpiration;
 
+    @Id
     @Digits(integer = 3, fraction = 0, message = "Invalid CVV")
     private String ccCVV;
 
