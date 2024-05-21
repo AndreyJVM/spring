fd80fdce89c8fd525916a1608193d412d4075cd8
Andrey Vorobev <Andrey.Vorobev.AQA@gmail.com>
2024-05-21 07:40:20 +0300

    impl Spring Data JDBC

--- spring/src/main/java/com/example/spring/data/IIngredientRepository.java
+++ spring/src/main/java/com/example/spring/data/IIngredientRepository.java
@@ -1,13 +1,15 @@
 package com.example.spring.data;
 
 import com.example.spring.tacos.Ingredient;
+import org.springframework.data.repository.CrudRepository;
+
 import java.util.Optional;
 
 /** Retrieving all ingredients as a collection of {@code Ingredient} objects.
  * Getting one ingredient by ID.
  * Saving an object {@code Ingredient}
  * */
-public interface IIngredientRepository {
+public interface IIngredientRepository extends CrudRepository<Ingredient, String> {
 
     Iterable<Ingredient> findAll();
 
--- spring/src/main/java/com/example/spring/data/IOrderRepository.java
+++ spring/src/main/java/com/example/spring/data/IOrderRepository.java
@@ -1,7 +1,8 @@
 package com.example.spring.data;
 
 import com.example.spring.tacos.TacoOrder;
+import org.springframework.data.repository.CrudRepository;
 
-public interface IOrderRepository {
+public interface IOrderRepository extends CrudRepository<TacoOrder, Long> {
     TacoOrder save(TacoOrder tacoOrder);
 }
