127d552c21bb00d697b630e5ddb34e136b5e2c3f
it <it@cus-an-01>
2024-05-30 16:21:16 +0300

    Update Taco.java, Ingredient.java

--- spring/src/main/java/com/example/spring/tacos/Ingredient.java
+++ spring/src/main/java/com/example/spring/tacos/Ingredient.java
@@ -1,12 +1,21 @@
 package com.example.spring.tacos;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /** У каждого ингредиента есть название и тип, позволяющие его визуально квалифицировать
  * */
 @Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PRIVATE, force = true)
 public class Ingredient {
 
+    @Id
     private final String id;
     private final String name;
     private final Type type;
--- spring/src/main/java/com/example/spring/tacos/Taco.java
+++ spring/src/main/java/com/example/spring/tacos/Taco.java
@@ -1,19 +1,32 @@
 package com.example.spring.tacos;
 
-import jakarta.validation.constraints.NotNull;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+
+import java.util.Date;
 import java.util.List;
 
 /** Класс помогающий понять как объединяются ингредиенты {@code Ingredient}
  * */
 @Data
+@Entity
 public class Taco {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     @Size(min=5, message = "Name must be at least 5 characters long")
     private String name;
 
-    @NotNull
+    private Date createAt = new Date();
+
+    @ManyToMany
     @Size(min = 1, message = "You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
+
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
+    }
 }
